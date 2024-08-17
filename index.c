4ace9125c31b2a61196120f502db4346d6385db9
ruth-chirwa <chirwaruth647@gmail.com>
2024-08-17 16:46:15 -0400

    my first hello world in C! exciting

---
+++ index.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+    printf("hello, world\n");
+    printf("I'm an Anspiring Software Developer. I am taking this journey to learn how to be a problem solver and a creative\n");
+}
