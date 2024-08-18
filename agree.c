d5da3a959c706fffcce24f07982549257089edac
Ruth <129673094+ruth-chirwa@users.noreply.github.com>
2024-08-18 11:26:13 +0000

    Sun, Aug 18, 2024, 7:26 AM -04:00

--- agree.c
+++ agree.c
@@ -3,14 +3,20 @@
 
 int main(void)
 {
-    char c = get_char("Do you like Ice cream? ");
+    char c = get_char("Do you like ice cream? ");
 
     if (c == 'y' || c == 'Y')
     {
-        printf("Yes I do!\n");
+        printf("Yes, I do!\n");
     }
     else if (c == 'n' || c == 'N')
     {
-        printf("No I don't\n");
+        printf("No, I don't.\n");
     }
+    else
+    {
+        printf("Invalid input. Please enter 'y', 'Y', 'n', or 'N'.\n");
+    }
+
+    return 0;
 }
