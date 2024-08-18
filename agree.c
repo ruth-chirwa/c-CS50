050aac188a4a352aa129f04d8420b17fa6328d49
Ruth <129673094+ruth-chirwa@users.noreply.github.com>
2024-08-18 11:16:13 +0000

    Sun, Aug 18, 2024, 7:16 AM -04:00

---
+++ agree.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    char c = get_char("Do you like Ice cream? ");
+
+    if (c == 'y')
+    {
+        printf("Yes I do!\n");
+    }
+    else (c == 'n')
+    {
+        printf("No I don't\n");
+    }
+}
