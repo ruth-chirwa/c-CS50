61de1f72e91b3654fbb73c84a99ff0eb94f181b3
Ruth <129673094+ruth-chirwa@users.noreply.github.com>
2024-08-18 18:49:38 +0000

    Sun, Aug 18, 2024, 2:49 PM -04:00

--- loop.c
+++ loop.c
@@ -1,13 +1,13 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-   int x = get_int("what is x");
-   int y = get_int("what is y");
+#include <stdio.h>
 
-   if (x < y)
-   {
-    printf("X is less than Y\n");
-   }
+int main() {
+    // For loop to print numbers from 1 to 5
+    for (int i = 1; i <= 5; i++) {
+        printf("%d\n", i);
+    }
+
+    return 0;
 }
