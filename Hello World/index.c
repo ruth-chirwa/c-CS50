6e138cee6566c2baf89511e753c23c21632d1567
ruth-chirwa <chirwaruth647@gmail.com>
2024-08-18 02:44:08 -0400

    new line

--- Hello World/index.c
+++ Hello World/index.c
@@ -2,4 +2,5 @@
 int main(void){
     printf("hello, world\n");
     printf("I'm an Anspiring Software Developer. I am taking this journey to learn how to be a problem solver and a creative\n");
+    printf("Finally, I get to be creative and build solutions!\n");
 }
