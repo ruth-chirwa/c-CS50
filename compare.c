db813afaa67ced20e266c5a645322ed5220cc5ad
Ruth <129673094+ruth-chirwa@users.noreply.github.com>
2024-08-18 10:03:49 +0000

    Sun, Aug 18, 2024, 6:03 AM -04:00

---
+++ compare.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int x = get_int("What is X? ");
+    int y = get_int("What is Y? ");
+
+    if (x < y)
+    {
+        printf("X is less than y\n");
+    }else
+    {
+        printf("X is greater than y\n");
+    }
+}
