15446aff9d2a19b99fa226e4cb050830b83884e1
Ruth <129673094+ruth-chirwa@users.noreply.github.com>
2024-08-19 20:57:57 +0000

    Mon, Aug 19, 2024, 4:57 PM -04:00

--- more.c
+++ more.c
@@ -1,9 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void meow(int n);
+
 int main(void)
 {
-    for (int i = 0; i > 10; i++)
+    meow(10);
+}
+
+void meow(int n)
+{
+    for (int i = 0; i < 10; i++)
     {
         printf("Meow\n");
     }
