2a35acd0aea50da548ca7868f3249274219e6b52
Ruth <129673094+ruth-chirwa@users.noreply.github.com>
2024-08-19 20:49:36 +0000

    Mon, Aug 19, 2024, 4:49 PM -04:00

--- loop.c
+++ loop.c
@@ -2,19 +2,7 @@
 #include <stdio.h>
 
 
-int main() {
-    // For loop to print numbers from 1 to 5
-    for (int i = 1; i <= 5; i++) {
-        printf("%d\n", i);
-    }
-
- int i = 1; // Initialize the loop variable
-
-    // While loop to print numbers from 1 to 5
-    while (i <= 5) {
-        printf("%d\n", i);
-        i++; // Increment the loop variable
-    }
-
-    return 0;
+int main(void)
+{
+    printf("meow\n");
 }
