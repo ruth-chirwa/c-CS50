3c620daa44f9351ba984c84074cf145e79a9e803
Ruth <129673094+ruth-chirwa@users.noreply.github.com>
2024-08-21 17:16:55 +0000

    Wed, Aug 21, 2024, 1:16 PM -04:00

--- calculator.c
+++ calculator.c
@@ -1,14 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int add(void);
+int add(int a, int b);
 int main(void)
 {
     printf("This is my calculator\n");
 
     int x = get_int("x: ");
     int y = get_int("y: ");
-    int z = add();
+    int z = add(x, y);
 
     printf("%i\n", z);
 }
